e546e4d3bf9c6f044fd8dfec61ddf7da53861095
Andreas Hertle <hertle@informatik.uni-freiburg.de>
2015-07-22 14:28:43 +0200

    added another node that sends an initial pose to amcl

--- src/m3rsm_global_localization/src/global_localization.cc
+++ src/m3rsm_global_localization/src/global_localization.cc
@@ -2,32 +2,50 @@
 
 
 void GlobalLocalization::processMap(const nav_msgs::OccupancyGrid& map_msg) {
-    map = (map_t*) malloc(sizeof(map_t));
+	if (map != NULL)
+	{
+		bool size_changed = false;
+	    size_changed |= map->size_x == map_msg.info.width;
+	    size_changed |= map->size_y == map_msg.info.height;
+	    size_changed |= map->scale == map_msg.info.resolution;
+	    size_changed |= map->origin_x == map_msg.info.origin.position.x + (map->size_x / 2) * map->scale;
+	    size_changed |= map->origin_y == map_msg.info.origin.position.y + (map->size_y / 2) * map->scale;
+	    if (size_changed)
+	    {
+	    	free(map->cells);
+	    	free(map);
+	    	map = NULL;
+	    }
+	}
+	if (map == NULL)
+	{
+		map = (map_t*) malloc(sizeof(map_t));
 
-    // Assume we start at (0, 0)
-    map->origin_x = 0;
-    map->origin_y = 0;
+		// Assume we start at (0, 0)
+		map->origin_x = 0;
+		map->origin_y = 0;
 
-    // Make the size odd
-    map->size_x = 0;
-    map->size_y = 0;
-    map->scale = 0;
+		// Make the size odd
+		map->size_x = 0;
+		map->size_y = 0;
+		map->scale = 0;
 
-    // Allocate storage for main map
-    map->cells = (map_cell_t*) NULL;
+		// Allocate storage for main map
+		map->cells = (map_cell_t*) NULL;
 
-    ROS_ASSERT(map);
+		ROS_ASSERT(map);
 
-    map->size_x = map_msg.info.width;
-    map->size_y = map_msg.info.height;
-    map->scale = map_msg.info.resolution;
-    map->origin_x = map_msg.info.origin.position.x + (map->size_x / 2) * map->scale;
-    map->origin_y = map_msg.info.origin.position.y + (map->size_y / 2) * map->scale;
+		map->size_x = map_msg.info.width;
+		map->size_y = map_msg.info.height;
+		map->scale = map_msg.info.resolution;
+		map->origin_x = map_msg.info.origin.position.x + (map->size_x / 2) * map->scale;
+		map->origin_y = map_msg.info.origin.position.y + (map->size_y / 2) * map->scale;
 
-    // Convert to amcl map format
-    map->cells = (map_cell_t*)malloc(
-            (sizeof(map_cell_t)) * map->size_x * map->size_y);
-    ROS_ASSERT(map->cells);
+		// Convert to amcl map format
+		map->cells = (map_cell_t*)malloc(
+				(sizeof(map_cell_t)) * map->size_x * map->size_y);
+		ROS_ASSERT(map->cells);
+	}
     for (int i = 0; i < map->size_x * map->size_y; ++i) {
         if (map_msg.data[i] == 0) {
             map->cells[i].occ_state = -1; // free
@@ -41,36 +59,46 @@ void GlobalLocalization::processMap(const nav_msgs::OccupancyGrid& map_msg) {
     map_update_cspace(map, max_occ_dist);
 
     ROS_INFO("Map received. Creating coarser likelyhood maps.");
-    field = new LikelyHoodField(map, sigma_hit, z_rand, 
-            z_hit, laser_max_range);
+    if (field != NULL)
+    {
+    	delete field;
+    }
+    field = new LikelyHoodField(map, sigma_hit, z_rand, z_hit, laser_max_range);
     field->initializeLikelyHoodFieldMap();
     map_frame_id = map_msg.header.frame_id;
     map_stamp = map_msg.header.stamp;
     
 }
 
-void GlobalLocalization::processLaserScan(
-        const sensor_msgs::LaserScan::ConstPtr& scan) {
-    LaserData d(scan, laser_max_range);
-    data = d;
+void GlobalLocalization::processLaserScan(const sensor_msgs::LaserScan::ConstPtr& scan) {
     ROS_INFO("Laser scan received");
-    tf::Pose localizedPose = globalLocalization();
-    ROS_INFO("Global pose is: %.3f %.3f %.3f",
-            localizedPose.getOrigin().x(),
-            localizedPose.getOrigin().y(),
-            tf::getYaw(localizedPose.getRotation()));
-    tf::Stamped<tf::Pose> stampedPose(
-            localizedPose, map_stamp, map_frame_id);
-    geometry_msgs::PoseStamped poseMsg;
-    tf::poseStampedTFToMsg(stampedPose, poseMsg);
-    pose_pub.publish(poseMsg);
+    try
+    {
+    	tf::Stamped<tf::Pose> localizedPose = globalLocalization(scan);
+		ROS_INFO("Global pose is: %.3f %.3f %.3f",
+				localizedPose.getOrigin().x(),
+				localizedPose.getOrigin().y(),
+				tf::getYaw(localizedPose.getRotation()));
+		geometry_msgs::PoseStamped poseMsg;
+		tf::poseStampedTFToMsg(localizedPose, poseMsg);
+		pose_pub.publish(poseMsg);
+    }
+    catch(...)
+    {
+    }
 }
 
-tf::Pose GlobalLocalization::globalLocalization() {
+tf::Stamped<tf::Pose> GlobalLocalization::globalLocalization(const sensor_msgs::LaserScan::ConstPtr& scan) {
+	if (field == NULL)
+	{
+		ROS_INFO_THROTTLE(1.0, "Waiting for map data...");
+		throw;
+	}
     ROS_INFO("Initiating global localization...");
+    data = LaserData(scan, laser_max_range);
     ros::Time start = ros::Time::now();
-    tf::Pose pose = field->likelyHoodFieldModel(data);
+	tf::Stamped<tf::Pose> stampedPose(field->likelyHoodFieldModel(data), scan->header.stamp, map_frame_id);
     double time_elapsed = (ros::Time::now() - start).toSec();
     ROS_INFO("Global localization took %.3f seconds", time_elapsed);
-    return pose;
+    return stampedPose;
 } 
---
+++ src/m3rsm_global_localization/src/global_localize_once.cc
@@ -0,0 +1,77 @@
+#include "global_localization.h"
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
+ros::Publisher pose_pub;
+bool localized = false;
+boost::shared_ptr<GlobalLocalization> glob;
+void processLaserScan(const sensor_msgs::LaserScan::ConstPtr& scan)
+{
+	// assumption: one AMCL node awaits initial location
+	// stop publishing after first publish
+	// resume publishing when AMCL node is restarted
+	if (pose_pub.getNumSubscribers() > 0)
+	{
+		if (! localized)
+		{
+			try
+			{
+				tf::Stamped<tf::Pose> pose = glob->globalLocalization(scan);
+				geometry_msgs::PoseWithCovarianceStamped msg;
+				msg.header.stamp = pose.stamp_;
+				msg.header.frame_id = pose.frame_id_;
+				tf::poseTFToMsg(pose, msg.pose.pose);
+				// covariance matrix
+				// [0.25, 0.0, 0.0, 0.0, 0.0, 0.0,
+				//  0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
+				//  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				//  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				//  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				//  0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
+				msg.pose.covariance.elems[0] = 0.25;
+				msg.pose.covariance.elems[7] = 0.25;
+				msg.pose.covariance.elems[35] = 0.06853891945200942;
+				pose_pub.publish(msg);
+				localized = true;
+			}
+			catch(...)
+			{
+			}
+		}
+	}
+	else
+	{
+		localized = false;
+	}
+}
+
+int main(int argc, char** argv) {
+    ROS_INFO("Waiting for map and laserscan messages...");
+    ros::init(argc, argv, "m3rsm_global_localization");
+
+    ros::NodeHandle nh;
+    double sigma_hit; 
+    double z_rand;
+    double z_hit;
+    double max_range;
+    nh.param("laser_sigma_hit", sigma_hit, 0.2);
+    nh.param("laser_z_rand", z_rand, 0.05);
+    nh.param("laser_z_hit", z_hit, 0.95);
+    nh.param("laser_max_range", max_range, 29.0);
+
+    pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>(
+            "localized_pose", 1, true );
+    glob.reset(new GlobalLocalization(sigma_hit, z_rand, z_hit, max_range, pose_pub));
+
+
+    // Request map
+    ros::Subscriber mapSub = nh.subscribe("map", 1, &GlobalLocalization::processMap, glob);
+
+   // Request laser scan message
+    ros::Subscriber scanSub = nh.subscribe<sensor_msgs::LaserScan>("frontLaser", 10, &processLaserScan);
+
+    ros::spin();
+    return 0;
+}
+
+
+
